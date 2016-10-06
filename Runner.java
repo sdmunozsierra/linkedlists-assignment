ac1b5bb8bf7abe3ad2cb87ece8f0f1a2fe9a91b2
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-06 13:29:51 -0600

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,21 +1,14 @@
 package lab4_linkedlists;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		Scanner scan = null;
-		File f = new File("file.txt");
-		try {
-			scan = new Scanner(f);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		
 		/* Step 1: Create a linked list */
 		System.out.println("Creating a linked list... ");
 		LinkedList linked = new LinkedList(); // Creates an empty linked list
@@ -23,6 +16,7 @@ public class Runner {
 		// Test if the linked list has any elements (should not)
 		System.out.println("Is linked list empty? -> " + linked.isEmpty() + "\n");
 
+		
 		/* Step 2: Create a box to test on the linked list */
 		System.out.println("Creating a Box... ");
 		Box testBox = new Box(1, 1, 1, 1);
@@ -31,6 +25,7 @@ public class Runner {
 		System.out.println("Volume of testBox: " + testBox.getVolume()); // get volume
 		System.out.println("Is testBox cube? -> " + testBox.isCube() + "\n"); // isCube
 
+		
 		/* Step 3: Insert the Box in the linked list */
 		System.out.println("Adding testBox to linked");
 		linked.addNodeFront(testBox);
@@ -42,58 +37,72 @@ public class Runner {
 		linked.printAllBoxes(); //Print all Boxes
 		
 		/* Step 5: Create more boxes from file */
-		//LinkedList linkedFile = createLinkedList(scan);
+		LinkedList linkedFile = createLinkedList();
+		System.out.println("is read file empty? -> "+linkedFile.isEmpty() );
 		
-		//System.out.println("is read file empty? -> "+linkedFile.isEmpty() );
-		
-		Box testBox2 = new Box(2, 2, 2);
-		Box testBox3 = new Box(3, 3, 3);
-		linked.addNode(testBox2);
-		linked.addNode(testBox3);
-		System.out.println("ADDED MORE BOXES");
+		//Print the contents of the linked list read file
+		linkedFile.printAllBoxes();
 		
 		
+		/* Step 6: Test Methods */
+		/** Print in Reverse */
+//		System.out.println("\nPrinting in Reverse...");
+//		linked.printReverse();
 		
-		linked.printAllBoxes();
-	
-		Box box2 = linked.getBox(0);
+		/* Box at position x */
+		Box box2 = linkedFile.getBox(2);
 		box2.printBox();
+	
+		/*Find the smallest element in a Linked List*/
+		System.out.println("SIZE: "+linkedFile.getSize());
+		findSmallest(linkedFile);
 		
-		//Find the smallest element in a Linked List
-		findSmallest(linked);
-		
+		System.out.println("SIZE: "+linkedFile.getSize());
 	}
 	
-	public static LinkedList createLinkedList(Scanner sc){
-		//Create a new Linked List
+	public static LinkedList createLinkedList() {
+		// Create a new Linked List
 		LinkedList tempLinked = new LinkedList();
 		
-		//Read line per line (Each box per line)
-		while(sc.hasNextLine()){
-            String line = sc.nextLine();
-            String[] dimensions = line.split(" ");
-            double w = Double.parseDouble(dimensions[0]);
-            double h = Double.parseDouble(dimensions[1]);
-            double l = Double.parseDouble(dimensions[2]);
-            
-            //Create a new Box
-            Box tempBox = new Box(w,h,l); //pass values
-            tempLinked.addNodeFront(tempBox); //add to linked list
-           
-            //While Repeat//
-        }
+		try {
+			// Open the file
+			FileInputStream fstream = new FileInputStream("file.txt");
+			// Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			// Read File Line By Line
+			while ((strLine = br.readLine()) != null) {
+				// split the line 
+				String[] dimensions = strLine.split(" "); 
+				double w = Double.parseDouble(dimensions[0]);
+				double h = Double.parseDouble(dimensions[1]);
+				double l = Double.parseDouble(dimensions[2]);
+				
+				//Create a box
+				Box tempBox = new Box(w, h, l); // pass values
+				tempLinked.addNode(tempBox); // add to linked list (in Order)
+				
+			}
+			// Close the input stream
+			in.close();
+		} catch (Exception e) {// Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+		//Return the linked list
 		return tempLinked;
 	}
 	
 	public static void findSmallest(LinkedList list){
 		Box smallest = list.getBox(0);
-		for(int i=1; i<list.getSize();i++){
+		for(int i=1; i<list.getSize()-1;i++){
 			if(smallest.getVolume()>list.getBox(i).getVolume()){
 				smallest = list.getBox(i);
 			}
 		}
-		System.out.println("INFORMATION-> "+ smallest.getVolume());
+		System.out.println("SMALLEST VOL-> "+ smallest.getVolume());
 		
 	}
 
 }
+
